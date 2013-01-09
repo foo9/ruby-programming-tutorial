51cb3cc3ba0f356acb0ecb9d30856475ce4f536a
foo9 <fu9fu9fu9@gmail.com>
2013-01-09 14:26:32 +0900

    Modify a message

--- chapter04/ex02.rb
+++ chapter04/ex02.rb
@@ -1,8 +1,9 @@
 # encoding: utf-8
 
-# 好きな数を入力してもらい、それに1を加えて、その結果を ベターな 数字として薦めるプログラムを書きましょう。 (機転の利いたサジェスチョンに努めましょう。)
+# 好きな数を入力してもらい、それに1を加えて、その結果を ベターな 数字として
+# 薦めるプログラムを書きましょう。 (機転の利いたサジェスチョンに努めましょう。)
 
-puts "Please your favorite number?"
+puts "Please enter your favorite numbers:"
 input_number = STDIN.gets.to_f
-num = (input_number + 1).to_i 
+num = (input_number + 1).to_i
 puts "Result: #{num} (Add number entered in the 1. It is an integer value.)"
